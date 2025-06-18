b8e74215192d80c1071c8a57932bb42c4221a71b
davlobos <carlos.dasilva.v@mail.pucv.cl>
2025-06-18 13:41:59 -0400

    Se sube ejemplo de archivo env
    archivo env de ejemplo que sirve para ingresar a la base de datos.

---
+++ env_example.js
@@ -0,0 +1,6 @@
+//Crea un archivo llamado "env.js" y copiar el código de abajo pero rellenar el URL y ANON KEY con las credenciales usadas en la base de datos.
+
+window.ENV = {
+  SUPABASE_URL: 'TU_URL_AQUI',
+  SUPABASE_ANON_KEY: 'TU_ANON_KEY_AQUI'
+};
